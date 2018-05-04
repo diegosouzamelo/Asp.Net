00544aad73d722bc900a9bd4e44a588942592e02
Diego <diegosouzarv16@hotmail.com>
2018-05-04 18:34:58 -0300

    Upload

---
+++ RecDiegoSouzaASP/Models/DataAccessObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RecDiegoSouzaASP.Models
+{/// <summary>
+/// Chamada do Banco Modelo para Tratar (DAO)
+/// </summary>
+    public class DataAccessObject:DbContext
+    {
+        public DataAccessObject() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<RecDiegoSouzaASP.Models.Category> Categories { get; set; }
+
+        public System.Data.Entity.DbSet<RecDiegoSouzaASP.Models.News> News { get; set; }
+
+    }
+}
---
+++ RecDiegoSouzaASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RecDiegoSouzaASP.Startup))]
+namespace RecDiegoSouzaASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
